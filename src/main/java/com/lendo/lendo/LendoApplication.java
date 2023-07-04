9009cfb6bc04e92d76234618309157f13f5e93f5
npvietkhoa <npvietkhoa@gmail.com>
2023-07-04 13:08:36 +0200

    Initial commit

---
+++ src/main/java/com/lendo/lendo/LendoApplication.java
@@ -0,0 +1,13 @@
+package com.lendo.lendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendoApplication.class, args);
+	}
+
+}
